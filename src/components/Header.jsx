53b324e401c101e247257eb67845eece263009cc
PlykinYurij <plikin.yurij@yandex.ru>
2022-09-19 14:44:53 +0400

    \

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+        <img src="https://61.img.avito.st/640x480/5680136361.jpg" />
+      </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img className="footerPicter" src="https://img2.goodfon.ru/original/3360x1050/1/8d/gory-vershiny-dolina-reka-les.jpg" />
+      </div>
+      <div>
+        ava + desp
+      </div>
+      <div>
+        my post
+        <div>
+          newPost
+        </div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  )
+}
+export default Profile;
