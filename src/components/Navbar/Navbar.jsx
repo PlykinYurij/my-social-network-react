9b268afc95627631d85fc5daa6ca385b86bc344d
PlykinYurij <plikin.yurij@yandex.ru>
2022-09-23 15:09:34 +0400

    /

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import classes from "./Navbar.module.css";
-
+import { NavLink } from "react-router-dom"
+const setActive = ({isActive}) => isActive ? classes.active : " ";
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={`${classes.item} ${classes.active}`}><a href="/profile">Profile</a></div>
-            <div className={classes.item}><a href="/dialogs">Messenge</a></div>
-            <div className={classes.item}><a href="news">News</a></div>
-            <div className={classes.item}><a href="music">Music</a></div>
-            <div className={classes.item}><a href="settings">Settings</a></div>
+            <div className={`${classes.item} ${classes.active}`}>
+                <NavLink to="/profile" className= {setActive} >Profile</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/dialogs" className= {setActive}>Messenge</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="news" className= {setActive}>News</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink  to="music" className= {setActive}>Music</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink  to="settings" className= {setActive}>Settings</NavLink>
+            </div>
 
         </nav>
 
