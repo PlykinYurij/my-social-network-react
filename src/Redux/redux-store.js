bbab3fc4e3b90380fcf5feddd93198b20b17e05f
PlykinYura <plikin.yurij@yandex.ru>
2022-11-04 19:08:52 +0400

    login, cookie

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,4 +1,5 @@
 import { combineReducers, legacy_createStore } from "redux";
+import authReducer from "./auth-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import usersReducer from "./users-reducer";
@@ -7,6 +8,7 @@ let redusers = combineReducers ({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
+    auth: authReducer,
 });
 
 let store = legacy_createStore(redusers); 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Header.module.css";
 
-const Header = () => {
-    return (
-        <header className={classes.header}>
-        <div><img src="https://it-yota.ru/upload/iblock/cc0/podklyuchenie_yota_4g.png" alt="logo"/></div>        
-      </header>
-    )
+const Header = (props) => {
+  return (
+    <header className={classes.header}>
+      <div><img src="https://it-yota.ru/upload/iblock/cc0/podklyuchenie_yota_4g.png" alt="logo" />
+        <div className={classes.loginBlock}>
+          {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+        </div>
+      </div>
+    </header>
+  )
 }
 export default Header;
